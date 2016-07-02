98e8275b7a98f790d52c22cca7986463ec5b6d29
= <taha.m.ansari@gmail.com>
2016-07-02 10:03:03 +0530

    added home page

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,5 +1,11 @@
 // Initialize app
-var myApp = new Framework7();
+var myApp = new Framework7({
+
+    modalTitle: 'My App',
+    material:true,
+    swipePanel:'left'
+
+});
 
 
 // If we need to use custom DOM library, let's save it to $$ variable:
@@ -8,36 +14,38 @@ var $$ = Dom7;
 // Add view
 var mainView = myApp.addView('.view-main', {
     // Because we want to use dynamic navbar, we need to enable it for this view:
-    dynamicNavbar: true
+    dynamicNavbar: true,
+    domCache:true
 });
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
+
 });
 
 
-// Now we need to run the code that will be executed only for About page.
+// // Now we need to run the code that will be executed only for About page.
 
-// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
+// // Option 1. Using page callback for page (for "about" page in this case) (recommended way):
+// myApp.onPageInit('about', function (page) {
+//     // Do something here for "about" page
 
-})
+// })
 
-// Option 2. Using one 'pageInit' event handler for all pages:
-$$(document).on('pageInit', function (e) {
-    // Get page data from event data
-    var page = e.detail.page;
+// // Option 2. Using one 'pageInit' event handler for all pages:
+// $$(document).on('pageInit', function (e) {
+//     // Get page data from event data
+//     var page = e.detail.page;
 
-    if (page.name === 'about') {
-        // Following code will be executed for page with data-page attribute equal to "about"
-        myApp.alert('Here comes About page');
-    }
-})
+//     if (page.name === 'about') {
+//         // Following code will be executed for page with data-page attribute equal to "about"
+//         myApp.alert('Here comes About page');
+//     }
+// })
 
-// Option 2. Using live 'pageInit' event handlers for each page
-$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
-    // Following code will be executed for page with data-page attribute equal to "about"
-    myApp.alert('Here comes About page');
-})
+// // Option 2. Using live 'pageInit' event handlers for each page
+// $$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+//     // Following code will be executed for page with data-page attribute equal to "about"
+//     myApp.alert('Here comes About page');
+// })
