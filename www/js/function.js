d8ba097970cafecb21f5bdb993463216167072c3
= <taha.m.ansari@gmail.com>
2016-08-12 22:55:42 +0530

    added features

---
+++ www/js/function.js
@@ -0,0 +1,25 @@
+$(document).on('click','#individual_signup_button',function(event){
+
+		event.preventDefault();
+		alert("clicked");
+
+
+
+
+});
+
+$(document).on('click','#dealer_signup_button',function(event){
+
+		event.preventDefault();
+		alert("clicked");
+
+
+});
+
+$(document).on('click','#login_button',function(event){
+
+		event.preventDefault();
+		alert("clicked");
+
+
+});
